c18fc7dd38b4cc66128607eed6156804c028f757
Alexandra Anderson <aliganderson@gmail.com>
2016-04-12 09:59:29 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use(express.static(path.join(__dirname, './static')));
+
+app.listen(8000, function(){
+	console.log('Listening on port 8000 for users and customers');
+});
